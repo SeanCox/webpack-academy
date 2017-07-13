48807149866252bfa6ee368dbe7271e8e85d716a
Sean Cox <sean@seans-mbp.attlocal.net>
2017-07-12 23:10:29 -0400

    finsihed

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /.jpe?g$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
